57aaf1f737aeaad6146ac99e143457861b624332
Ak-erke <144439688+Ak-erke@users.noreply.github.com>
2023-11-09 16:52:33 +0600

    regex.py

--- lab7/regex.py
+++ lab7/regex.py
@@ -4,30 +4,30 @@ with open('row.txt', 'r', encoding='utf-8') as file:
     data = file.read()
 
 #task1
-matches = re.findall(r'a[b]*', data)
+matches = re.findall(r'а[б]*', data)
 print(matches)
 
 print()#task2
 
-pattern1 = r'ab{2,3}'
+pattern1 = r'аб{2,3}'
 matches1 = re.findall(pattern1, data)
 print(matches1)
 
 print()#task3
 
-pattern2 = r'[a-z]+_[a-z]+'
+pattern2 = r'[а-я]+_[а-я]+'
 matches2 = re.findall(pattern2, data)
 print(matches2)
 
 print()#task4
 
-pattern3 = r'[A-Z][a-z]+'
+pattern3 = r'[А-Я][А-Я]+'
 matches3 = re.findall(pattern3, data)
 print(matches3)
 
 print()#task5
 
-pattern4 = r'a.*b'
+pattern4 = r'а.*б'
 matches4 = re.findall(pattern4, data)
 print(matches4)
 
@@ -48,7 +48,7 @@ print(camel_case_string)
 
 print()#task8
 
-pattern7 = r'[A-Z]'
+pattern7 = r'[А-Я]'
 splits = re.split(pattern7, data)
 print(splits)
 #for split in splits:
@@ -56,14 +56,14 @@ print(splits)
 
 print()#task9
 
-pattern8 = r'(?<=[a-z])(?=[A-Z])'
+pattern8 = r'(?<=[а-я])(?=[А-Я])'
 result = re.sub(pattern8, ' ', data)
 print(result)
 
 print()#task10
 
 def camel_to_snake(camel_str):
-    return re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', camel_str).lower()
+    return re.sub(r'([а-я0-9])([А-Я])', r'\1_\2', camel_str).lower()
 
 snake_case_string = camel_to_snake(data)
 print(snake_case_string)
